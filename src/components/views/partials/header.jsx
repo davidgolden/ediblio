cc934dcccd8100ade77360da80fac93200e2005c
David Golden <David@David-mac.local>
2018-01-25 20:30:06 -0700

    minor

--- src/components/views/partials/header.jsx
+++ src/components/views/partials/header.jsx
@@ -105,7 +105,7 @@ class Header extends React.Component {
   render() {
     return (
       <nav className="navbar navbar-expand-lg navbar-light">
-        <a className="navbar-brand" href="/">Recipe Cloud <i className="fas fa-cloud"></i></a>
+        <a className="navbar-brand" href="/">Recipe Cloud yeee <i className="fas fa-cloud"></i></a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
