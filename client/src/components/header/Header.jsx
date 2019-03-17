ce13294e7b2a0b43f7c255baaf1051ab9dbbf2d2
David Golden <david@soprisapps.com>
2019-03-17 08:34:47 -0600

    add mobile nav

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -4,7 +4,7 @@ import {inject, observer} from 'mobx-react';
 import classNames from 'classnames';
 import styles from './styles/Header.scss';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faCloud, faSearch, faListUl, faPlus, faBook, faUser} from '@fortawesome/free-solid-svg-icons';
+import {faCloud, faSearch, faListUl, faPlus, faBook, faUser, faHamburger} from '@fortawesome/free-solid-svg-icons';
 import Button from "../utilities/buttons/Button";
 
 @inject('apiStore')
@@ -14,7 +14,8 @@ export default class Header extends React.Component {
         super(props);
         this.state = {
             email: '',
-            password: ''
+            password: '',
+            showNav: false,
         }
     }
 
@@ -31,6 +32,14 @@ export default class Header extends React.Component {
 
     handleLogout = () => {
         this.props.apiStore.userLogout();
+    };
+
+    toggleNav = () => {
+        this.setState(prevState => {
+            return {
+                showNav: !prevState.showNav,
+            }
+        })
     };
 
     componentDidMount() {
@@ -50,6 +59,13 @@ export default class Header extends React.Component {
         const logoutClassName = classNames({
             [styles.logout]: true,
         });
+        const linksContainerClassName = classNames({
+            [styles.linksContainer]: true,
+            [styles.linksContainerVisible]: this.state.showNav,
+        });
+        const mobileHamClassName = classNames({
+            [styles.mobileHam]: true,
+        });
 
         return (
             <nav className={navContainerClassName}>
@@ -57,48 +73,54 @@ export default class Header extends React.Component {
                     <a href="/">Recipe Cloud <FontAwesomeIcon icon={faCloud}/></a>
                 </h1>
 
-                <div>
-                    {this.props.apiStore.isLoggedIn && <ul>
-                        <li>
-                            <Link to={'/'} className={headerLinkClassName}>
-                                <FontAwesomeIcon icon={faSearch}/>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to={`/users/${this.props.apiStore.user._id}/groceries`}
-                                  className={headerLinkClassName}>
-                                <FontAwesomeIcon icon={faListUl}/>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to={'/add'} className={headerLinkClassName}>
-                                <FontAwesomeIcon icon={faPlus}/>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to={`/users/${this.props.apiStore.user._id}/recipes`} className={headerLinkClassName}>
-                                <FontAwesomeIcon icon={faBook}/>
-                            </Link>
-                        </li>
-                    </ul>}
-                    {this.props.apiStore.isLoggedIn ? (<React.Fragment>
-                        <Link className={userLinkClassName} to={`/users/${this.props.apiStore.user._id}/settings`}>
-                            <FontAwesomeIcon icon={faUser}/> <span>{this.props.apiStore.user.username}</span>
-                        </Link>
-                        <Button className={logoutClassName} onClick={this.handleLogout}>Log Out</Button>
-                    </React.Fragment>) : (<React.Fragment>
-                        <input type="email" name="email" placeholder='Email' className=''
-                               value={this.state.email}
-                               onChange={this.handleEmailChange}/>
-                        <input type="password" name="password" placeholder='Password'
-                               value={this.state.password}
-                               onChange={this.handlePasswordChange}/>
-                        <Button className={logoutClassName} onClick={this.handleLoginSubmit}
-                                value='Login'>Login</Button>
+                <Button onClick={this.toggleNav} className={mobileHamClassName}>
+                    <FontAwesomeIcon icon={faHamburger}/>
+                </Button>
 
-                        {/*<Link className={userLinkClassName} to={'/forgot'}>Forgot Password?</Link>*/}
-                        <Link className={userLinkClassName} to={'/register'}>Register</Link>
-                    </React.Fragment>)}
+                <div className={linksContainerClassName}>
+                    {this.props.apiStore.isLoggedIn ? <React.Fragment>
+                            <ul>
+                                <li>
+                                    <Link to={'/'} className={headerLinkClassName}>
+                                        <FontAwesomeIcon icon={faSearch}/>
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to={`/users/${this.props.apiStore.user._id}/groceries`}
+                                          className={headerLinkClassName}>
+                                        <FontAwesomeIcon icon={faListUl}/>
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to={'/add'} className={headerLinkClassName}>
+                                        <FontAwesomeIcon icon={faPlus}/>
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to={`/users/${this.props.apiStore.user._id}/recipes`}
+                                          className={headerLinkClassName}>
+                                        <FontAwesomeIcon icon={faBook}/>
+                                    </Link>
+                                </li>
+                            </ul>
+                            <Link className={userLinkClassName} to={`/users/${this.props.apiStore.user._id}/settings`}>
+                                <FontAwesomeIcon icon={faUser}/> <span>{this.props.apiStore.user.username}</span>
+                            </Link>
+                            <Button className={logoutClassName} onClick={this.handleLogout}>Log Out</Button>
+                        </React.Fragment> :
+                        <React.Fragment>
+                            <input type="email" name="email" placeholder='Email'
+                                   value={this.state.email}
+                                   onChange={this.handleEmailChange}/>
+                            <input type="password" name="password" placeholder='Password'
+                                   value={this.state.password}
+                                   onChange={this.handlePasswordChange}/>
+                            <Button className={logoutClassName} onClick={this.handleLoginSubmit}
+                                    value='Login'>Login</Button>
+
+                            {/*<Link className={userLinkClassName} to={'/forgot'}>Forgot Password?</Link>*/}
+                            <Link className={userLinkClassName} to={'/register'}>Register</Link>
+                        </React.Fragment>}
                 </div>
             </nav>
         )
