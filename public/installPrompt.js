4f33a6f17b5c9ac91213d508932a521c8def3008
David Golden <davidgoldeninbox@gmail.com>
2020-01-18 12:27:54 -0700

    only show install when can be installed

--- public/installPrompt.js
+++ public/installPrompt.js
@@ -2,6 +2,7 @@ var deferredPrompt;
 window.addEventListener('beforeinstallprompt', function(e) {
     deferredPrompt = e;
     var installBtn = document.querySelector('#installAppButton');
+    installBtn.style.display = 'inline-block';
     installBtn.addEventListener("click", function(e) {
         installBtn.style.display = 'none';
         deferredPrompt.prompt();
