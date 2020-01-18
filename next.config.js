67b2bf90fe17eed405818d6c3cadf54283f405cb
David Golden <davidgoldeninbox@gmail.com>
2020-01-18 16:49:56 -0700

    ..

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@ const withSass = require('@zeit/next-sass');
 const withCSS = require('@zeit/next-css');
 const withOffline = require('next-offline');
 
+const CACHE_NAME = "1.0.0";
+
 module.exports = withCSS(withSass(withOffline({
     cssModules: true,
     cssLoaderOptions: {
@@ -37,4 +39,19 @@ module.exports = withCSS(withSass(withOffline({
 
         return config;
     },
+    workboxOpts: {
+        runtimeCaching: [{
+            urlPattern: /\/.*$/,
+            handler: 'StaleWhileRevalidate',
+            options: {
+                cacheName: CACHE_NAME + "-pages",
+                expiration: {
+                    maxAgeSeconds: 60 * 60 * 24 * 30, // 30 days
+                },
+                cacheableResponse: {
+                    statuses: [0, 200],
+                },
+            },
+        }]
+    }
 })));
