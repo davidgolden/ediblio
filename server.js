8dfc1d2921a08ad76aa21e2a8cb610581da7b9a9
David Golden <davidgoldeninbox@gmail.com>
2020-02-06 08:50:52 -0700

    select grocery list and refactor so we only write this query once

--- server.js
+++ server.js
@@ -35,33 +35,47 @@ const forceSsl = function (req, res, next) {
 
 const SESS_LIFETIME = 1000 * 60 * 60 * 24 * 30;
 
+const usersSelector = `
+SELECT users.*, users.id::int,
+COALESCE(json_agg(m) FILTER (WHERE m IS NOT NULL), '[]') menu,
+COALESCE(json_agg(c) FILTER (WHERE c IS NOT NULL), '[]') collections,
+COALESCE(json_agg(g) FILTER (WHERE g IS NOT NULL), '[]') grocery_list
+FROM users
+LEFT JOIN LATERAL (
+SELECT * FROM recipes
+WHERE recipes.id IN (
+SELECT id FROM users_recipes_menu
+WHERE users_recipes_menu.user_id = users.id
+)
+) m ON true
+LEFT JOIN LATERAL (
+SELECT collections.*, json_agg(cr) recipes
+FROM collections, LATERAL (
+SELECT * FROM recipes
+WHERE recipes.id IN (
+SELECT recipe_id FROM recipes_collections
+WHERE recipes_collections.collection_id = collections.id
+)
+) cr
+WHERE collections.author_id = users.id
+GROUP BY collections.id
+) c ON true
+LEFT JOIN LATERAL (
+SELECT *
+FROM ingredients
+WHERE ingredients.id IN (
+SELECT user_id FROM users_ingredients_groceries
+WHERE users_ingredients_groceries.user_id = users.id
+)
+) g ON true
+`;
+
 //Configure Passport
 passport.use(new LocalStrategy({
     usernameField: 'email',
     passwordField: 'password'
 }, async function (email, password, done) {
-    const userRes = await db.query({text: `
-        select users.*, users.id::int,
-COALESCE(json_agg(m) FILTER (WHERE m IS NOT NULL), '[]') menu,
-COALESCE(json_agg(c) FILTER (WHERE c IS NOT NULL), '[]') collections
-from users LEFT JOIN LATERAL (
-	select recipes.* from recipes
-	where recipes.id in (
-		select id from users_recipes_menu
-		where users_recipes_menu.user_id = users.id
-	)
-) m ON true LEFT JOIN LATERAL (
-	select collections.*, json_agg(cr) recipes
-	from collections, LATERAL (
-		select * from recipes
-		where recipes.id in (
-			select recipe_id from recipes_collections
-			where recipes_collections.collection_id = collections.id
-		)
-	) cr
-	where collections.author_id = users.id
-	group by collections.id
-) c ON true
+    const userRes = await db.query({text: `${usersSelector}
 where users.email = $1
 group by users.id;`, values: [email]});
     const user = userRes.rows[0];
@@ -95,28 +109,7 @@ passport.serializeUser(function (user, done) {
 });
 
 passport.deserializeUser(async function (id, done) {
-    const userRes = await db.query({text: `
-        select users.*, users.id::int,
-COALESCE(json_agg(m) FILTER (WHERE m IS NOT NULL), '[]') menu,
-COALESCE(json_agg(c) FILTER (WHERE c IS NOT NULL), '[]') collections
-from users LEFT JOIN LATERAL (
-	select recipes.* from recipes
-	where recipes.id in (
-		select id from users_recipes_menu
-		where users_recipes_menu.user_id = users.id
-	)
-) m ON true LEFT JOIN LATERAL (
-	select collections.*, json_agg(cr) recipes
-	from collections, LATERAL (
-		select * from recipes
-		where recipes.id in (
-			select recipe_id from recipes_collections
-			where recipes_collections.collection_id = collections.id
-		)
-	) cr
-	where collections.author_id = users.id
-	group by collections.id
-) c ON true
+    const userRes = await db.query({text: `${usersSelector}
 where users.id = $1
 group by users.id;`, values: [id]});
     done(null, userRes.rows[0]);
