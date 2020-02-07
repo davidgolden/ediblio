751628becebaac5408c8c4a647e5a43e2828d58d
David Golden <davidgoldeninbox@gmail.com>
2020-02-07 07:39:58 -0700

    add ratings to table creation

--- pg-server.js
+++ pg-server.js
@@ -21,6 +21,11 @@ function populate() {
                         VALUES ('${user.username}', '${user.email}', '${user.profileImage}', '${user.password}') RETURNING *;
                     `);
 
+                    await db.query({
+                        text: `INSERT INTO collections (name, author_id, is_primary) VALUES ($1, $2, $3)`,
+                        values: ['Favorites', userRes.rows[0].id, true]
+                    });
+
                     // add all user's created recipes
                     await Recipe.find({author_id: user._id}, async (err, recipes) => {
                         for (let i = 0; i < recipes.length; i++) {
@@ -45,16 +50,27 @@ function populate() {
                             }
                         }
                     });
+                }
 
-                    await Rating.find({author_id: user._id}, async (err, ratings) => {
-                        for (let i = 0; i < ratings.length; i++) {
+                await Rating.find({})
+                    .populate('recipe_id')
+                    .populate('author_id')
+                    .exec(async (err, ratings) => {
+                        for (let x = 0; x < ratings.length; x++) {
+                            const recipeRes = await db.query({
+                                text: 'SELECT id FROM recipes WHERE name = $1',
+                                values: [ratings[x].recipe_id.name]
+                            });
+                            const userRes = await db.query({
+                                text: 'SELECT id FROM users where email = $1',
+                                values: [ratings[x].author_id.email]
+                            });
                             await db.query({
-                                text: 'INSERT INTO ratings (recipe_id, author_id, rating) VALUES ($1, $2, $3) RETURNING *',
-                                values: [ratings[i].recipe_id, userRes.rows[0].id, ratings[i].rating],
+                                text: 'INSERT INTO ratings (recipe_id, author_id, rating) VALUES ($1, $2, $3)',
+                                values: [recipeRes.rows[0].id, userRes.rows[0].id, ratings[x].rating],
                             })
                         }
-                    })
-                }
+                    });
 
                 await Collection.find({})
                     .populate('recipes')
@@ -91,7 +107,7 @@ function populate() {
 }
 
 async function create() {
-    db.query(`
+    await db.query(`
     CREATE TABLE users (
         id BIGSERIAL NOT NULL PRIMARY KEY,
         created_at DATE NOT NULL DEFAULT CURRENT_DATE,
@@ -120,31 +136,40 @@ async function create() {
     );
     CREATE TABLE recipes_collections (
         id BIGSERIAL NOT NULL PRIMARY KEY,
+        created_at DATE NOT NULL DEFAULT CURRENT_DATE,
         collection_id BIGSERIAL REFERENCES collections(id) ON DELETE CASCADE,
         recipe_id BIGSERIAL REFERENCES recipes(id) ON DELETE CASCADE
     );
     CREATE TABLE ingredients (
         id BIGSERIAL NOT NULL PRIMARY KEY,
+        created_at DATE NOT NULL DEFAULT CURRENT_DATE,
         name TEXT NOT NULL,
         quantity TEXT NOT NULL,
         measurement TEXT NOT NULL
     );
     CREATE TABLE recipes_ingredients (
         id BIGSERIAL NOT NULL PRIMARY KEY,
+        created_at DATE NOT NULL DEFAULT CURRENT_DATE,
         recipe_id BIGSERIAL REFERENCES recipes(id) ON DELETE CASCADE,
         ingredient_id BIGSERIAL REFERENCES ingredients(id) ON DELETE CASCADE
     );
     CREATE TABLE users_recipes_menu (
         id BIGSERIAL NOT NULL PRIMARY KEY,
+        created_at DATE NOT NULL DEFAULT CURRENT_DATE,
+        deleted BOOLEAN DEFAULT FALSE,
         user_id BIGSERIAL REFERENCES users(id) ON DELETE CASCADE,
         recipe_id BIGSERIAL REFERENCES recipes(id) ON DELETE CASCADE
     );
     CREATE TABLE users_ingredients_groceries (
         id BIGSERIAL NOT NULL PRIMARY KEY,
+        created_at DATE NOT NULL DEFAULT CURRENT_DATE,
+        deleted BOOLEAN DEFAULT FALSE,
         user_id BIGSERIAL REFERENCES users(id) ON DELETE CASCADE,
         ingredient_id BIGSERIAL REFERENCES ingredients(id) ON DELETE CASCADE
     );
     CREATE TABLE users_collections_followers (
+        id BIGSERIAL NOT NULL PRIMARY KEY,
+        created_at DATE NOT NULL DEFAULT CURRENT_DATE,
         user_id BIGSERIAL REFERENCES users(id) ON DELETE CASCADE,
         collection_id BIGSERIAL REFERENCES users(id) ON DELETE CASCADE
     );
@@ -153,18 +178,11 @@ async function create() {
         created_at DATE NOT NULL DEFAULT CURRENT_DATE,
         recipe_id BIGSERIAL REFERENCES users(id) ON DELETE CASCADE,
         author_id BIGSERIAL REFERENCES users(id) ON DELETE CASCADE,
-        rating INT NOT NULL
+        rating REAL NOT NULL
     );
-`, (err, res) => {
-        if (err) {
-            return console.log('err: ', err);
-        }
-        console.log('success');
-    });
-
+`);
 }
 
 create()
-    .then(populate)
-    .then(() => console.log('done'));
+    .then(populate);
 // after running this must run  psql recipecloud -p 5433 < node_modules/connect-pg-simple/table.sql to get sessions working
