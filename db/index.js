78d8d351a4981552bd70f2bb3c3199f9d497146f
David Golden <davidgoldeninbox@gmail.com>
2020-02-16 14:18:37 -0700

    add password

--- db/index.js
+++ db/index.js
@@ -1,6 +1,7 @@
 const { Pool } = require('pg');
 const pool = new Pool({
     user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
     host: process.env.PGHOST,
     port: process.env.PGPORT,
     database: process.env.PGDATABASE
