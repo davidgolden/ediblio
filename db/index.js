5af93e6d331762952c33ce1928cddf63044fea9b
David Golden <davidgoldeninbox@gmail.com>
2020-02-16 14:11:36 -0700

    update pg configs

--- db/index.js
+++ db/index.js
@@ -1,5 +1,10 @@
 const { Pool } = require('pg');
-const pool = new Pool();
+const pool = new Pool({
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    database: process.env.PGDATABASE
+});
 module.exports = {
     query: (text, params, callback) => {
         return pool.query(text, params, callback)
