157b415463edf19377e15ac63608929904f4cc29
David Golden <davidgoldeninbox@gmail.com>
2020-03-15 08:30:42 -0600

    change no user image default

--- client/components/header/Header.jsx
+++ client/components/header/Header.jsx
@@ -203,7 +203,7 @@ const Header = observer((props) => {
                     <button onClick={() => setNavOpen(v => !v)} className={userLinkClassName}>
                         {context.user.profile_image ?
                             <img src={context.user.profile_image}/> :
-                            <FontAwesomeIcon icon={faUser}/>} <FontAwesomeIcon icon={faChevronDown}/>
+                            <div><FontAwesomeIcon icon={faUser}/></div>} <FontAwesomeIcon icon={faChevronDown}/>
                     </button>
                     : <button className={userLinkClassName} onClick={() => context.addModal("login")}>
                         Login
