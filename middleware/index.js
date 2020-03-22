41664abd2f55c46db54d8c5554b8dc243bc911a3
David Golden <davidgoldeninbox@gmail.com>
2020-03-22 14:33:15 -0600

    refactor middleware to work :)

--- middleware/index.js
+++ middleware/index.js
@@ -13,47 +13,56 @@ middlewareObj.isLoggedIn = (req, res, next) => {
 
 middlewareObj.checkRecipeOwnership = async (req, res, next) => {
   // is user logged in?
-  middlewareObj.isLoggedIn(req, res, next);
-  const response = await db.query({
-    text: `SELECT * FROM recipes WHERE id = $1 AND author_id = $2`,
-    values: [req.params.recipe_id, req.user.id]
-  });
+  if (req.user) {
+    const response = await db.query({
+      text: `SELECT * FROM recipes WHERE id = $1 AND author_id = $2`,
+      values: [req.params.recipe_id, req.user.id]
+    });
 
-  if (response.rows.length === 0) {
+    if (response.rows.length === 0) {
+      return res.status(404).send({ detail: "You don't have permission to do that!" })
+    }
+
+    next();
+  } else {
     return res.status(404).send({ detail: "You don't have permission to do that!" })
   }
-
-  next();
 };
 
 middlewareObj.checkIngredientOwnership = async (req, res, next) => {
   // is user logged in?
-  middlewareObj.isLoggedIn(req, res, next);
-  const response = await db.query({
-    text: `SELECT * FROM users_ingredients_groceries WHERE id = $1 AND user_id = $2`,
-    values: [req.params.ingredient_id, req.user.id]
-  });
+  if (req.user) {
+    const response = await db.query({
+      text: `SELECT * FROM users_ingredients_groceries WHERE id = $1 AND user_id = $2`,
+      values: [req.params.ingredient_id, req.user.id]
+    });
 
-  if (response.rows.length === 0) {
+    if (response.rows.length === 0) {
+      return res.status(404).send({ detail: "You don't have permission to do that!" })
+    }
+
+    next();
+  } else {
     return res.status(404).send({ detail: "You don't have permission to do that!" })
   }
-
-  next();
 };
 
 middlewareObj.checkCollectionOwnership = async (req, res, next) => {
   // is user logged in?
-  middlewareObj.isLoggedIn(req, res, next);
-  const response = await db.query({
-    text: `SELECT * FROM collections WHERE id = $1 AND author_id = $2`,
-    values: [req.params.collection_id, req.user.id]
-  });
+  if (req.user) {
+    const response = await db.query({
+      text: `SELECT * FROM collections WHERE id = $1 AND author_id = $2`,
+      values: [req.params.collection_id, req.user.id]
+    });
 
-  if (response.rows.length === 0) {
+    if (response.rows.length === 0) {
+      return res.status(404).send({ detail: "You don't have permission to do that!" })
+    }
+
+    next();
+  } else {
     return res.status(404).send({ detail: "You don't have permission to do that!" })
   }
-
-  next();
 };
 
 module.exports = middlewareObj;
