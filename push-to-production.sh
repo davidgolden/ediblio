7b86011e8cccc8558903cfa05057def48a1d0a91
davidgolden <david@soprisapps.com>
2022-11-12 17:10:01 -0700

    update build script

--- push-to-production.sh
+++ push-to-production.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ssh root@144.126.212.244 'cd /var/www/ediblio; git pull; ./release-to-production.sh'
+ssh root@144.126.212.244 'cd /var/www/ediblio; git pull; ./update-prod.sh'
---
+++ update-prod.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+while getopts ":i" option; do
+   case $option in
+      i)
+         npm cache clean --force
+         npm cache verify
+
+         echo "installing dependencies"
+         npm ci
+   esac
+done
+
+echo "building next"
+npm run build
+
+echo "restarting process"
+pm2 restart server
+pm2 restart ediblio
