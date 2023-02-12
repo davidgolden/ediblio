43a4f4097d07c94be496027c95304ea8d367758c
davidgolden <david@soprisapps.com>
2023-02-12 08:41:58 -0700

    add and delete ingredients to prisma

--- pages/api/recipes/[recipe_id]/ingredients.js
+++ pages/api/recipes/[recipe_id]/ingredients.js
@@ -1,4 +1,4 @@
-import db from "../../../../db";
+import {prismaClient} from "../../../../db";
 import {checkRecipeOwnership} from "../../../../utils/serverUtils";
 
 export default async function handler(req, res) {
@@ -7,18 +7,15 @@ export default async function handler(req, res) {
         try {
             await checkRecipeOwnership(req, res);
 
-            const response = await db.query({
-                text: `
+            const recipes = await prismaClient.$queryRaw`
                 INSERT INTO recipes_ingredients (recipe_id, name, measurement_id, quantity)
-                VALUES ($1,
-                    $2,
-                    (SELECT id FROM measurements WHERE short_name = $3),
-                    $4
-                ) RETURNING *`,
-                values: [req.query.recipe_id, req.body.name, req.body.measurement, req.body.quantity],
-            });
+                VALUES (${req.query.recipe_id}::uuid,
+                    ${req.body.name},
+                    (SELECT id FROM measurements WHERE short_name = ${req.body.measurement}),
+                    ${req.body.quantity}
+                ) RETURNING *;`;
 
-            return res.status(200).send(response.rows[0]);
+            return res.status(200).send(recipes[0]);
         } catch (error) {
             res.status(404).send({detail: error.message});
         }
@@ -27,15 +24,13 @@ export default async function handler(req, res) {
         try {
             await checkRecipeOwnership(req, res);
 
-            await db.query({
-                text: `
+            await prismaClient.$queryRaw`
                 DELETE FROM recipes_ingredients
-                WHERE id IN (${req.body.ingredient_ids.map(id => "'" + id + "'").join(", ")})
-                `,
-            });
+                WHERE id IN (${req.body.ingredient_ids.join(", ")}::uuid);`;
 
             res.status(200).send();
         } catch (error) {
+            console.error(error);
             res.status(404).send({detail: error.message});
         }
     }
