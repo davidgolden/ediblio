0630a66c94ea39be10a877769991170e8e0e7d1d
davidgolden <david@soprisapps.com>
2023-02-12 19:58:17 -0700

    post rating to prisma

--- pages/api/rating.js
+++ pages/api/rating.js
@@ -1,4 +1,4 @@
-import db from "../../db/index";
+import {prismaClient} from "../../db/index";
 import {getUserIdFromRequest} from "../../utils/serverUtils";
 
 export default async function handler(req, res) {
@@ -6,41 +6,27 @@ export default async function handler(req, res) {
         try {
             const userId = getUserIdFromRequest(req);
 
-            await db.query("BEGIN");
-            const exists = await db.query({
-                text: `SELECT * FROM ratings WHERE recipe_id = $1 AND author_id = $2`,
-                values: [req.body.recipe_id, userId],
-            });
+            await prismaClient.$transaction(async (tx) => {
+                const exists = await tx.$queryRaw`SELECT * FROM ratings WHERE recipe_id = ${req.body.recipe_id}::uuid AND author_id = ${userId}::uuid;`
 
-            // edit or create new rating
-            let rating;
-            if (exists.rows.length > 0) {
-                // exists
-                rating = await db.query({
-                    text: `UPDATE ratings SET rating = $1 WHERE id = $2 RETURNING *`,
-                    values: [req.body.rating, exists.rows[0].id],
+                // edit or create new rating
+                let rating;
+                if (exists.length > 0) {
+                    // exists
+                    rating = await tx.$queryRaw`UPDATE ratings SET rating = ${req.body.rating} WHERE id = ${exists[0].id}::uuid RETURNING *;`
+                } else {
+                    rating = await tx.$queryRaw`INSERT INTO ratings (recipe_id, author_id, rating) VALUES (${req.body.recipe_id}, ${userId}::uuid, ${req.body.rating}) RETURNING *;`
+                }
+
+                const average = await tx.$queryRaw`SELECT avg(rating) FROM ratings WHERE recipe_id = ${req.body.recipe_id}::uuid GROUP BY id;`
+
+                return res.status(200).json({
+                    rating: rating[0],
+                    avg_rating: average[0].avg,
                 });
-            } else {
-                rating = await db.query({
-                    text: `INSERT INTO ratings (recipe_id, author_id, rating) VALUES ($1, $2, $3) RETURNING *`,
-                    values: [req.body.recipe_id, userId, req.body.rating],
-                })
-            }
+            })
 
-            const average = await db.query({
-                text: `SELECT avg(rating) FROM ratings WHERE recipe_id = $1 GROUP BY id`,
-                values: [req.body.recipe_id],
-            });
-
-            await db.query('COMMIT');
-
-
-            return res.status(200).json({
-                rating: rating.rows[0],
-                avg_rating: average.rows[0].avg,
-            });
         } catch (error) {
-            await db.query('ROLLBACK');
             return res.status(404).send(error);
         }
     }
